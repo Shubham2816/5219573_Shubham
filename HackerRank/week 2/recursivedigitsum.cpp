2ba67ad3a1e9785a40274f0b26024c43a751e883
Shubham Kumar <Shubham2816kumar@gmail.com>
2025-08-21 18:07:05 +0530

    Save work before pull

---
+++ HackerRank/week 2/recursivedigitsum.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
+int superDigitHelper(long long x) {
+    if (x < 10) return x;
+    long long s = 0;
+    while (x > 0) {
+        s += x % 10;
+        x /= 10;
+    }
+    return superDigitHelper(s);
+}
+
+int superDigit(string n, int k) {
+long long s = 0;
+    
+    for (char c : n) {
+        s += (c - '0');
+    }
+   
+    s *= k;
+
+    return superDigitHelper(s);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    string n = first_multiple_input[0];
+
+    int k = stoi(first_multiple_input[1]);
+
+    int result = superDigit(n, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
